4d27c593134b4bd2673de78f6e77fbc7c8367fb2
Tamara Serrano <84792523+Tamara729@users.noreply.github.com>
2022-08-08 03:42:04 -0400

    Week_9_Rscript.R
    Uploaded Week 9 (amended analysis plan) with steps as comments

---
+++ Week_9_Code_Rscript.R
@@ -0,0 +1,99 @@
+### Week 9 Assignment: Revisit Analysis Plan ###
+
+### Amended for publication figures featuring yield mass, canopy area, GNDVI, and NDVI across six N applications 
+
+
+
+#Clear the console and set working directory
+
+#Load packages
+
+#Import harvest data and canopy data
+
+#Select variables of interest, mutate variables to SI units, extract unit from ID
+
+#Convert unit into a numeric factor
+
+#Combine harvest and canopy data into a new dataframe
+
+#Filter data by variety
+
+#Group by unit and fertility treatment
+
+#Summarize mean values (yield, canopy, NDVI, and GNDVI) and their confidence intervals for each plot
+
+#Summarize mean values (yield, canopy, NDVI, and GNDVI) and their confidence intervals for the six N treatments
+
+
+
+####Yield vs. N application figure####
+
+#Data Visualization
+
+#ANOVA model and duncan.test
+
+#Assign groupings for yield
+
+#Combine data with groupings, select unique variables
+
+#Create bargraph with confidence intervals and letters
+
+
+
+####Canopy area vs. N application figure#### 
+
+#ANOVA model and duncan.test
+
+#Assign groupings for canopy area
+
+#Combine data with groupings, select unique variables
+
+#Create bargraph with confidence intervals and letters
+
+
+
+####NDVI reflectance values vs. N application figure####
+
+#ANOVA model and duncan.test
+
+#Assign groupings for NDVI 
+
+#Combine data with groupings, select unique variables
+
+#Create bargraph with confidence intervals and letters
+
+
+
+#####GNDVI reflectance values vs. N application figure####
+
+#ANOVA model and duncan.test
+
+#Assign groupings for GNDVI
+
+#Combine data with groupings, select unique variables
+
+#Create bargraph with confidence intervals and letters
+
+
+
+#Yield~NDVI
+
+#log transformation of yield data, fitting regression model
+
+#graphing log transformed yield and NDVI values
+
+
+
+#Yield~GNDVI
+#log transformation of yield data, fitting regression model
+
+#graphing log transformed yield and GNDVI values
+
+
+
+#Yield~Canopy
+#log transformation of yield data, fitting regression model
+
+#graphing log transformed yield and canopy area 
+
+
